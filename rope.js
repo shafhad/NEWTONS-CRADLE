e0a10b9c574b8fd344020cbbe1a908c848c0783e
shafhad <66770462+shafhad@users.noreply.github.com>
2020-11-08 20:47:37 +0530

    Update rope.js

--- rope.js
+++ rope.js
@@ -12,9 +12,9 @@ class Rope{
         
     }
     display(){
-        var Anchor1=this.rope.bodyA.position
+       // var Anchor1=this.rope.bodyA.position
        // var Anchor2x=this.rope.bodyB.position.x+this.offsetX
        // var Anchor2y=this.rope.bodyB.position.y+this.offsetY
-        line(Anchor1.x,Anchor1.y, Anchor2x,Anchor2y)  
+       // line(Anchor1.x,Anchor1.y, Anchor2x,Anchor2y)  
     }
 }
